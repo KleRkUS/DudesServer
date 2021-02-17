c36b247df432410d61451902b9dac32bf8fd72ad
KleRkUS <villevald.vladislav.sleep@gmail.com>
2021-02-17 09:48:33 +0300

    Fix

--- api/routes.ts
+++ api/routes.ts
@@ -1,7 +1,9 @@
 let router = require('express').Router();
 const uuid = require('node-uuid');
 const SessionController = require('./controllers/SessionController');
+const UserController = require('./controllers/UserController');
 
-router.post('/session/create', SessionController.createSession);
+
+router.post('/users/create', UserController.createTempUserDocument);
 
 module.exports = router;
